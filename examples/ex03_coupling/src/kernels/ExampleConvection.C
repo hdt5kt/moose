0a54e7a824b876de04849e87d33c45fe6ff78426
hdt5kt <hdt5kt@virginia.edu>
2025-05-25 18:52:45 -0500

    gradient operator

--- examples/ex03_coupling/src/kernels/ExampleConvection.C
+++ examples/ex03_coupling/src/kernels/ExampleConvection.C
@@ -9,29 +9,9 @@
 
 #include "ExampleConvection.h"
 
-// Don't forget to register your object with MOOSE
-registerMooseObject("ExampleApp", ExampleConvection);
-
-InputParameters
-ExampleConvection::validParams()
-{
-  InputParameters params = Kernel::validParams();
-
-  // Here we specify a new parameter for our kernel allowing users to indicate which other
-  // variable they want to be coupled into this kernel from an input file.
-  params.addRequiredCoupledVar(
-      "some_variable", "The gradient of this variable will be used as the velocity vector.");
-
-  return params;
-}
-
-ExampleConvection::ExampleConvection(const InputParameters & parameters)
-  : Kernel(parameters),
-    // using the user-specified name for the coupled variable, retrieve and store a reference to the
-    // coupled variable.
-    _grad_some_variable(coupledGradient("some_variable"))
-{
-}
+// Don't forget to register your object wiclearriable, retrieve and store a reference to the
+// coupled variable.
+_grad_some_variable(coupledGradient("some_variable")) {}
 
 Real
 ExampleConvection::computeQpResidual()
